e9e190077d2846d1489b9c1fcbde0828f72aabfc
Carlos Pleitez <cmpleitez.sv@gmail.com>
2025-08-10 21:44:09 -0600

    sync

--- database/migrations/2025_08_04_173039_add_role_id_to_users_table.php
+++ database/migrations/2025_08_04_173039_add_role_id_to_users_table.php
@@ -8,7 +8,7 @@ return new class extends Migration {
     public function up(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->foreignId('role_id')->default(7)->after('area_id');
+            $table->foreignId('role_id')->default(7);
             $table->foreign('role_id')->references('id')->on('roles');
         });
     }
