24f1603698de0d73a1e236bbbb8c1b030b73fbf8
Carlos Pleitez <cmpleitez.sv@gmail.com>
2025-08-16 18:50:25 -0600

    Limpiar repositorio: remover vendor y build del control de versiones

--- public/build/assets/LivIconsEvo.defaults-ujxDFHYS.js
+++
@@ -1,1 +0,0 @@
-if(typeof jQuery>"u")throw window.alert("LivIcons Evolution Script requires jQuery (https://jquery.com)!"),new Error("LivIcons Evolution Script requires jQuery (https://jquery.com)!");
--- public/build/assets/form-validation-E5UUpa3n.js
+++
@@ -1,1 +0,0 @@
-(function(n,o,t){t("input,select,textarea").not("[type=submit]").jqBootstrapValidation()})(window,document,jQuery);
