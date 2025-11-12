49ce7d8b719ab0bc9f9cde5ce84a20e0d33ea1f0
Carlos Pleitez <cmpleitez.sv@gmail.com>
2025-11-11 21:46:09 -0600

    actualizando compras y stocks

--- app/Http/Controllers/ProductoController.php
+++ app/Http/Controllers/ProductoController.php
@@ -46,48 +46,79 @@ class ProductoController extends Controller
         } catch (\Illuminate\Validation\ValidationException $e) {
             return back()->with('error', 'Error en la validación: ' . $e->getMessage());
         }
+        //agregar validación de unidades para que cuadren los stocks
+
+        if ($validated->origen_stock_id==1 && $validated->destino_stock_id==5) { //Validando cuando sean compras o devoluciones sobre compras
+            $origenStockUnidades = 0;
+            $destinoStockUnidades = $validated->unidades;
+        } else if ($validated->origen_stock_id==5 && $validated->destino_stock_id==1) {
+            $origenStockUnidades = $validated->unidades;
+            $destinoStockUnidades = 0;
+        } else { //Validando cuando sean movimientos entre stocks
+            $origenStockUnidades = $validated->unidades;
+            $destinoStockUnidades = $validated->unidades;
+        }
+
         //PROCESOS
-        try {
-            DB::beginTransaction();
-                
-                //POR CADA MOVIMIENTO HAY QUE ACTUALIZAR DOS STOCKS (ORIGEN Y DESTINO)
+        //try {
+            //DB::beginTransaction();
+                $origenStockName = Stock::findOrfail($validated->origen_stock_id)->stock; //Nombre del stock de origen
             
-                $oficinaStock = OficinaStock::where('oficina_id', auth()->user()->oficina_id)->where('stock_id', $request->origen_stock_id)->where('producto_id', $request->producto_id)->with('stock')->first();
-                
-                
-                $origenStockName = Stock::findOrfail($request->origen_stock_id)->stock;
-                
-                if (!$oficinaStock) {
-                    $oficinaStock = new OficinaStock();
-                    $oficinaStock->oficina_id = auth()->user()->oficina_id;
-                    $oficinaStock->stock_id = $request->destino_stock_id;
-                    $oficinaStock->producto_id = $request->producto_id;
-                    $oficinaStock->unidades = $request->unidades;
-                    $oficinaStock->save();
+                $oficinaStockOrigen = OficinaStock::where('oficina_id', auth()->user()->oficina_id) //Origen
+                ->where('stock_id', $validated->origen_stock_id)
+                ->where('producto_id', $validated->producto_id)
+                ->with('stock')
+                ->first();
+                if (!$oficinaStockOrigen) {
+                    $oficinaStockOrigen = new OficinaStock();
+                    $oficinaStockOrigen->oficina_id = auth()->user()->oficina_id;
+                    $oficinaStockOrigen->stock_id = $validated->destino_stock_id;
+                    $oficinaStockOrigen->producto_id = $validated->producto_id;
+                    $oficinaStockOrigen->unidades = $validated->unidades; //agregar excepcion: origen->1|5; destino->5|1
+                    $oficinaStockOrigen->save();
                 } else {
-                    $oficinaStock->unidades += $request->unidades;
-                    $oficinaStock->save();
+                    $oficinaStockOrigen->unidades -= $validated->unidades; //agregar excepcion: origen->1|5; destino->5|1
+                    $oficinaStockOrigen->save();
                 }
-                $movimiento = new Movimiento();
-                $movimiento->id = app(KeyMaker::class)->generate('Movimiento', $oficinaStock->stock_id);
+
+                $oficinaStockDestino = OficinaStock::where('oficina_id', auth()->user()->oficina_id) //Destino
+                ->where('stock_id', $validated->destino_stock_id)
+                ->where('producto_id', $validated->producto_id)
+                ->with('stock')
+                ->first();
+                if (!$oficinaStockDestino) {
+                    $oficinaStockDestino = new OficinaStock();
+                    $oficinaStockDestino->oficina_id = auth()->user()->oficina_id;
+                    $oficinaStockDestino->stock_id = $validated->destino_stock_id;
+                    $oficinaStockDestino->producto_id = $validated->producto_id;
+                    $oficinaStockDestino->unidades = $validated->unidades; //agregar excepcion: origen->5|1; destino->1|5
+                    $oficinaStockDestino->save();
+                } else {
+                    $oficinaStockDestino->unidades += $validated->unidades; //agregar excepcion: origen->5|1; destino->1|5
+                    $oficinaStockDestino->save();
+                }
+
+                $movimiento = new Movimiento(); //Movimiento
+                $movimiento->id = app(KeyMaker::class)->generate('Movimiento', $oficinaStockOrigen->stock_id);
                 $movimiento->user_id = auth()->id();
                 $movimiento->oficina_id = auth()->user()->oficina_id;
-                $movimiento->origen_stock_id = $request->origen_stock_id;
-                $movimiento->destino_stock_id = $oficinaStock->stock_id;
-                $movimiento->producto_id = $oficinaStock->producto_id;
-                $movimiento->movimiento = $origenStockName . ' -> ' . $oficinaStock->stock->stock;
-                $movimiento->unidades = $oficinaStock->unidades;
+                $movimiento->origen_stock_id = $validated->origen_stock_id;
+                $movimiento->destino_stock_id = $oficinaStockOrigen->stock_id;
+                $movimiento->producto_id = $oficinaStockOrigen->producto_id;
+                $movimiento->movimiento = $origenStockName . ' -> ' . $oficinaStockOrigen->stock->stock;
+                $movimiento->unidades = $oficinaStockOrigen->unidades;
                 $movimiento->save();
-            DB::commit();
+            //DB::commit();
             return back()->with('success', 'Movimiento registrado correctamente');
+/*
         } catch (QueryException $e) {
             DB::rollBack();
             return back()->with('error', 'Error en la consulta: ' . $e->getMessage());
         } catch (Exception $e) {
             DB::rollBack();
-            return back()->with('error', 'Error en la consulta: ' . $e->getMessage());
+            return back()->with('error', 'Error en la transacción: ' . $e->getMessage());
         }
-        
+*/        
     }
 
     public function store(Request $request)
