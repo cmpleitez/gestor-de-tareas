f91c6265e232b5224b9a4004f0724f5fb933e8a7
cmpleitez <cmpleitez.sv@gmail.com>
2025-11-19 08:17:44 -0600

    sync

--- app/Models/User.php
+++ app/Models/User.php
@@ -26,6 +26,7 @@ class User extends Authenticatable implements MustVerifyEmail
         'password',
         'role_id',
         'profile_photo_path',
+        'oficina_id',
     ];
     protected $hidden = [
         'password',
