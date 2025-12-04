9fc25d8ae8dd9c842721c6cc3a0fc025d00c50d1
cmpleitez <cmpleitez.sv@gmail.com>
2025-12-04 16:04:57 -0600

    ...edición de kits, productos y equivalentes

--- resources/views/modelos/kit/edit.blade.php
+++ resources/views/modelos/kit/edit.blade.php
@@ -1,7 +1,6 @@
 @extends('dashboard')
 
 @section('css')
-
 <style>
     .acordion-header {
         background-color: rgb(255, 255, 255) !important;
@@ -14,9 +13,28 @@
         display: flex;
         align-items: center;
     }
-
+    .row-equivalentes {
+        display: flex;
+        flex-wrap: wrap;
+    }
+    .row-equivalentes > div[class*="col-"] {
+        display: flex;
+        flex-direction: column;
+    }
+    .row-equivalentes .card {
+        height: 100%;
+        display: flex;
+        flex-direction: column;
+    }
+    .row-equivalentes .card-content {
+        flex: 1;
+        display: flex;
+        flex-direction: column;
+    }
+    .row-equivalentes .card-body {
+        flex: 1;
+    }
 </style>
-
 @stop
 
 @section('contenedor')
@@ -50,43 +68,40 @@
                         <div class="row mb-2"> {{-- Kit --}}
                             <div class="col-12 col-md-6 d-flex align-items-center justify-content-center">
                                 @if ($kit->image_path && Storage::disk('public')->exists($kit->image_path))
-                                <img src="{{ Storage::url($kit->image_path) }}" alt="avatar" style="height: 22em; width: 35em; object-fit: cover;">
+                                    <img src="{{ Storage::url($kit->image_path) }}" alt="avatar" style="height: 22em; width: 54em; object-fit: cover;">
                                 @else
-                                <img src="{{ asset('app-assets/images/pages/operador.png') }}" alt="avatar" style="height: 22em; width: 35em; object-fit: contain;">
+                                    <img src="{{ asset('app-assets/images/pages/operador.png') }}" alt="avatar" style="height: 22em; width: 19em; object-fit: contain;">
                                 @endif
                             </div>
                             <div class="col-12 col-md-6">
-                                <div class="card-body">
-                                    <div class="row">
-                                        <div class="col-md-12">
-                                            <div class="form-group">
-                                                <label for="kit">Kit</label>
-                                                <input type="text" name="kit" id="kit" class="form-control {{ $errors->has('kit') ? 'is-invalid' : '' }}" data-validation-required-message="Este campo es obligatorio" data-validation-containsnumber-regex="^(?! )[a-zA-ZáéíóúÁÉÍÓÚñÑ()]+( [a-zA-ZáéíóúÁÉÍÓÚñÑ()]+)*$" data-validation-containsnumber-message="Solo se permiten letras y paréntesis, sin espacios al inicio/final ni dobles espacios" data-validation-minlength-message="El nombre debe tener al menos 3 caracteres" data-clear="true" minlength="3" placeholder="Nombre del kit" value="{{ old('kit', $kit->kit) }}" required>
-                                                <div class="help-block"></div>
-                                                @error('kit')
-                                                <div class="col-sm-12 badge bg-danger text-wrap" style="margin-top: 0.2rem;">
-                                                    {{ $errors->first('kit') }}
-                                                </div>
-                                                @enderror
+                                <div class="row">
+                                    <div class="col-md-12">
+                                        <div class="form-group">
+                                            <label for="kit">Kit</label>
+                                            <input type="text" name="kit" id="kit" class="form-control {{ $errors->has('kit') ? 'is-invalid' : '' }}" data-validation-required-message="Este campo es obligatorio" data-validation-containsnumber-regex="^(?! )[a-zA-ZáéíóúÁÉÍÓÚñÑ()]+( [a-zA-ZáéíóúÁÉÍÓÚñÑ()]+)*$" data-validation-containsnumber-message="Solo se permiten letras y paréntesis, sin espacios al inicio/final ni dobles espacios" data-validation-minlength-message="El nombre debe tener al menos 3 caracteres" data-clear="true" minlength="3" placeholder="Nombre del kit" value="{{ old('kit', $kit->kit) }}" required>
+                                            <div class="help-block"></div>
+                                            @error('kit')
+                                            <div class="col-sm-12 badge bg-danger text-wrap" style="margin-top: 0.2rem;">
+                                                {{ $errors->first('kit') }}
                                             </div>
-                                            <div class="form-group">
-                                                <label>Fotografia del Kit <small class="text-muted">(Máximo 10 MB, solo
-                                                        JPEG/PNG)</small></label>
-                                                <input type="file" name="image_path" class="form-control" style="padding-bottom: 35px;" accept="image/jpeg,image/jpg,image/png" onchange="validateFileSize(this, 10)">
-                                                <small class="form-text text-muted">Formatos permitidos: JPEG, JPG, PNG. Tamaño
-                                                    máximo: 10 MB</small>
-                                                @error('image_path')
-                                                <div class="col-sm-12 badge bg-danger text-wrap" style="margin-top: 0.2rem;">
-                                                    {{ $errors->first('image_path') }}
-                                                </div>
-                                                @enderror
+                                            @enderror
+                                        </div>
+                                        <div class="form-group">
+                                            <label>Fotografia del Kit <small class="text-muted">(Máximo 10 MB, solo
+                                                    JPEG/PNG)</small></label>
+                                            <input type="file" name="image_path" class="form-control" style="padding-bottom: 35px;" accept="image/jpeg,image/jpg,image/png" onchange="validateFileSize(this, 10)">
+                                            <small class="form-text text-muted">Formatos permitidos: JPEG, JPG, PNG. Tamaño
+                                                máximo: 10 MB</small>
+                                            @error('image_path')
+                                            <div class="col-sm-12 badge bg-danger text-wrap" style="margin-top: 0.2rem;">
+                                                {{ $errors->first('image_path') }}
                                             </div>
+                                            @enderror
                                         </div>
                                     </div>
                                 </div>
                             </div>
                         </div>
-
                         <div class="row"> {{-- Productos --}}
                             @foreach ($kit->productos as $index => $producto)
                                 @php
@@ -94,47 +109,43 @@
                                     $headingId = 'heading' . $kitProducto_id;
                                     $accordionId = 'accordion' . $kitProducto_id;
                                 @endphp
-                                <div class="row" style="margin-bottom: 0.5em">
-                                    <div class="col-12 col-md-1 text-center"> {{-- Unidades --}}
+                                <div class="row" style="margin-bottom: 0.5em; width: 1920px; max-width: 100%;">
+                                    <div class="col-6 col-md-1 col-lg-1 col-xl-1 items-center justify-content-center"> {{-- Unidades --}}
                                         <input type="hidden" name="producto[{{ $kitProducto_id }}][producto_id]" value="{{ $producto->id }}">
                                         <input style="text-align: center;" type="number" name="producto[{{ $kitProducto_id }}][unidades]" id="producto_{{ $kitProducto_id }}_unidades" class="form-control" value="{{ old('producto.' . $kitProducto_id . '.unidades', $producto->kitProductos->first()->unidades) }}" required>
+                                        @error('producto[{{ $kitProducto_id }}][producto_id]')
+                                            <div class="col-sm-12 badge bg-danger text-wrap" style="margin-top: 0.2rem;">
+                                                {{ $errors->first('producto[' . $producto->id . '][unidades]') }}
+                                            </div>
+                                        @enderror
                                     </div>
-                                    <div class="col-12 col-md-1 text-center"> {{-- Botón de nuevo equivalente --}}
+                                    <div class="col-6 col-md-1 col-lg-1 col-xl-1text-center"> {{-- Botón de nuevo equivalente --}}
                                         <button type="button" class="btn btn-warning" data-toggle="modal" data-target="#modal-nuevo-equivalente" data-kit-producto-id="{{ $kitProducto_id }}" style="width: 100%; max-width: 100%; box-sizing: border-box;">
                                             <i class="bx bx-plus" style="top:0.1em !important; left:-0.6em !important;">e</i>
                                         </button>
                                     </div>
-                                    <div class="col-12 col-md-10"> {{-- Titulo del producto --}}
+                                    <div class="col-12 col-md-10 col-lg-10 col-xl-10"> {{-- Titulo del producto --}}
                                         <div class="accordion collapse-icon accordion-icon-rotate">
-                                            <div class="collapse-header" style="background-color: #f8f9fa !important; min-height: 2em;">
+                                            <div class="collapse-header" style="background-color:rgb(255, 255, 255) !important; min-height: 2em;">
                                                 <div id="{{ $headingId }}" class="acordion-header" data-toggle="collapse" data-target="#{{ $accordionId }}" aria-expanded="false" aria-controls="{{ $accordionId }}" role="tablist">
                                                     ID:{{ $kitProducto_id }} - Producto_id:{{ $producto->id }} - nombre:{{ $producto->producto }}
                                                 </div>
                                                 <div id="{{ $accordionId }}" role="tabpanel" aria-labelledby="{{ $headingId }}" class="collapse">
-                                                    <div class="row"> {{-- Equivalentes --}}
+                                                    <div class="row row-equivalentes"> {{-- Equivalentes --}}
                                                         @foreach ($producto->kitProductos->first()->equivalentes as $equivalente)
-                                                        <div class="col-md-3 col-sm-6 mb-sm-1">
-                                                            <div class="card">
-                                                                <div class="card-content">
-                                                                    <img class="card-img-top img-fluid" src="{{ asset('app-assets/images/pages/operador.png') }}" alt="Producto alterno" />
-                                                                    <div class="card-body">
-                                                                        <h4 class="card-title">ID:{{ $equivalente->kit_producto_id }} - Producto_id:{{ $equivalente->producto_id }} - nombre:{{ optional($equivalente->producto)->producto ?? 'N/A' }}</h4>
-                                                                        <p class="card-text">
-                                                                            This card has supporting text below as a natural lead-in to
-                                                                            additional content.
-                                                                        </p>
-                                                                        <small class="text-muted">Last updated 3 mins ago</small>
+                                                            <div class="col-sm-6 col-md-4 col-lg-3" style="padding-top: 0.5em;">
+                                                                <div class="card">
+                                                                    <div class="card-content">
+                                                                        <img class="card-img-top img-fluid" src="{{ asset('app-assets/images/pages/operador.png') }}" alt="Producto alterno" />
+                                                                        <div class="card-body" style="padding: 1em;">
+                                                                            <small class="text-muted">ID:{{ $equivalente->kit_producto_id }} - Producto_id:{{ $equivalente->producto_id }} - nombre:{{ $equivalente->producto->producto }}</small>
+                                                                        </div>
                                                                     </div>
                                                                 </div>
                                                             </div>
-                                                        </div>
                                                         @endforeach
                                                     </div>
-                                                    @error('producto[' . $producto->id . '][unidades]')
-                                                    <div class="col-sm-12 badge bg-danger text-wrap" style="margin-top: 0.2rem;">
-                                                        {{ $errors->first('producto[' . $producto->id . '][unidades]') }}
-                                                    </div>
-                                                    @enderror
+
                                                 </div>
                                             </div>
                                         </div>
@@ -189,11 +200,11 @@
                                 </div>
                             </div>
                             <div class="modal-footer">
-                                <button type="button" class="btn btn-light-primary" data-dismiss="modal">
+                                <button type="button" class="btn btn-light-warning" data-dismiss="modal">
                                     <i class="bx bx-x d-block d-sm-none"></i>
                                     <span class="d-none d-sm-block">Close</span>
                                 </button>
-                                <button type="submit" class="btn btn-primary ml-1">
+                                <button type="submit" class="btn btn-warning ml-1">
                                     <i class="bx bx-check d-block d-sm-none"></i>
                                     <span class="d-none d-sm-block">Accept</span>
                                 </button>
