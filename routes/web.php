a4cedb79f44fc8616bddff6aba47c13a06999d31
cmpleitez <cmpleitez.sv@gmail.com>
2025-12-15 09:22:48 -0600

    ...Maquetación de la vista contenido del carrito

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Http\Request;
+use Illuminate\Foundation\Auth\EmailVerificationRequest;
+
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\EquipoController;
 use App\Http\Controllers\ProductoController;
